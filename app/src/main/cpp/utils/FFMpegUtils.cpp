2b4e594e8500b0173a80e2f63ed4af5595f38d18
alaiye <alaiye_work@163.com>
2023-05-10 19:46:36 +0800

    fix paths have collided prob

--- app/src/main/cpp/utils/FFMpegUtils.cpp
+++ app/src/main/cpp/utils/FFMpegUtils.cpp
@@ -7,7 +7,7 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_xyq_ffmpegdemo_utils_FFMpegUtils_getVideoFramesCore(JNIEnv *env, jobject thiz,
                                                              jstring path, jint width, jint height,
-                                                             jobject cb) {
+                                                             jboolean precise, jobject cb) {
     // path
     const char *c_path = env->GetStringUTFChars(path, nullptr);
     std::string s_path = c_path;
@@ -25,20 +25,40 @@ Java_com_xyq_ffmpegdemo_utils_FFMpegUtils_getVideoFramesCore(JNIEnv *env, jobjec
     assert(jclazz != nullptr);
 
     jmethodID onFetchStart = env->GetMethodID(jclazz, "onFetchStart", "(D)[D");
-    jmethodID onProgress = env->GetMethodID(jclazz, "onProgress", "(Ljava/nio/ByteBuffer;DIII)Z");
+    jmethodID onProgress = env->GetMethodID(jclazz, "onProgress", "(Ljava/nio/ByteBuffer;DIIII)Z");
     jmethodID onFetchEnd = env->GetMethodID(jclazz, "onFetchEnd", "()V");
 
-    auto *reader = new FFVideoReader(s_path);
-    // todo w/h scale not impl
-    if (width <= 0 || height <= 0) {
-        width = reader->getMediaInfo().width;
-        height = reader->getMediaInfo().height;
+    auto *reader = new FFVideoReader();
+    reader->enableSkipNonRefFrame(true);
+    reader->init(s_path);
+
+    int videoWidth = reader->getMediaInfo().width;
+    int videoHeight = reader->getMediaInfo().height;
+    if (width <= 0 && height <= 0) {
+        width = videoWidth;
+        height = videoHeight;
+    } else if (width > 0 && height <= 0) { // scale base width
+        width += width % 2;
+        if (width > videoWidth) {
+            width = videoWidth;
+        }
+        height = (jint)(1.0 * width * videoHeight / videoWidth);
+        height += height % 2;
+    } else if (height > 0 && width <= 0) { // scale base height
+        height += height % 2;
+        if (height > videoHeight) {
+            height = videoHeight;
+        }
+        width = (jint)(1.0 * height * videoWidth / videoHeight);
     }
+    LOGI("video size: %dx%d, get frame size: %dx%d", videoWidth, videoHeight, width, height)
 
     auto timestamps = (jdoubleArray)env->CallObjectMethod(cb, onFetchStart, reader->getDuration());
     jdouble *tsArr = env->GetDoubleArrayElements(timestamps, nullptr);
     int size = env->GetArrayLength(timestamps);
-    LOGE("timestamps size: %d", size)
+    LOGI("timestamps size: %d", size)
+
+    int rotate = reader->getRotate();
 
     for (int i = 0; i < size; i++) {
         jobject jByteBuffer = env->CallObjectMethod(thiz, allocateFrame, width, height);
@@ -46,8 +66,8 @@ Java_com_xyq_ffmpegdemo_utils_FFMpegUtils_getVideoFramesCore(JNIEnv *env, jobjec
         memset(buffer, 0, width * height * 4);
 
         auto pts = (int64_t)tsArr[i];
-        reader->getFrame(pts, width, height, buffer);
-        jboolean abort = !env->CallBooleanMethod(cb, onProgress, jByteBuffer, tsArr[i], width, height, i);
+        reader->getFrame(pts, width, height, buffer, precise);
+        jboolean abort = !env->CallBooleanMethod(cb, onProgress, jByteBuffer, tsArr[i], width, height, rotate, i);
         if (abort) {
             LOGE("onProgress abort")
             break;
